ce7545d5ff2026cb651ab8df2ce0a0c879682972
billzhou2008 <billzhou2005@gmail.com>
2022-01-08 12:35:11 +0800

    improve network

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : NetworkBehaviour
 {
     public GameObject Canvas;
-    public GameObject DropZone;
+    public PlayerManager PlayerManager;
 
     private bool isDragging = false;
+    private bool isDraggable = true;
     private GameObject startParent;
     private GameObject dropZone;
     private Vector2 startPosition;
@@ -16,7 +18,9 @@ public class DragDrop : MonoBehaviour
     void Start()
     {
         Canvas = GameObject.Find("Main Canvas");
-        DropZone = GameObject.Find("DropZone");
+        if (!hasAuthority) { 
+            isDraggable = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -32,15 +36,21 @@ public class DragDrop : MonoBehaviour
         isOverDropZone=false;
         dropZone = null;
     }
-    public void StartDrag() { 
+    public void StartDrag() {
+        if (!isDraggable) return;
         isDragging = true;
         startParent = transform.parent.gameObject;
         startPosition = transform.position;
     }
     public void EndDrag() { 
+        if(!isDraggable) return;
         isDragging=false;
         if (isOverDropZone) {
             transform.SetParent(dropZone.transform,false);
+            isDraggable = false;
+            NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+            PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+            PlayerManager.PlayCard(gameObject);
         } else { 
             transform.position = startPosition;
             transform.SetParent(startParent.transform, false);
--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -1,28 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class DrawCards : MonoBehaviour
+public class DrawCards : NetworkBehaviour
 {
-    public GameObject card1;
-    public GameObject card2;
-    public GameObject PlayerArea;
-
+    public PlayerManager PlayerManager;
     public void onClick() {
-        for (int i = 0; i < 5; i++) {
-            GameObject card = Instantiate(card1, new Vector2(0, 0), Quaternion.identity);
-            card.transform.SetParent(PlayerArea.transform, false);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
     }
 }
